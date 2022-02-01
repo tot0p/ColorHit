a9742ede1b356762c1061d75bd4c4e34e39e67cd
lemaitre <Thomas.lemaitre.78420@gmail.com>
2022-02-01 21:50:15 +0100

    up

--- dep/player.go
+++ dep/player.go
@@ -1,6 +1,7 @@
 package dep
 
 import (
+	"fmt"
 	"math"
 	"math/rand"
 
@@ -14,22 +15,20 @@ type Player struct {
 	Map                         *Map
 	ImgData, ImgData1           *ebiten.Image
 	Angle                       int
+	Angle2                      int
 }
 
 func (p *Player) Update() {
 	if (ebiten.IsKeyPressed(ebiten.KeyA) || ebiten.IsKeyPressed(ebiten.KeyArrowLeft)) && p.X > 0 {
 		p.X -= velocity
 		p.Angle = 270
-	}
-	if (ebiten.IsKeyPressed(ebiten.KeyD) || ebiten.IsKeyPressed(ebiten.KeyArrowRight)) && p.X < p.RW-p.Width {
+	} else if (ebiten.IsKeyPressed(ebiten.KeyD) || ebiten.IsKeyPressed(ebiten.KeyArrowRight)) && p.X < p.RW-p.Width {
 		p.X += velocity
 		p.Angle = 90
-	}
-	if (ebiten.IsKeyPressed(ebiten.KeyW) || ebiten.IsKeyPressed(ebiten.KeyArrowUp)) && p.Y > 0 {
+	} else if (ebiten.IsKeyPressed(ebiten.KeyW) || ebiten.IsKeyPressed(ebiten.KeyArrowUp)) && p.Y > 0 {
 		p.Y -= velocity
 		p.Angle = 0
-	}
-	if (ebiten.IsKeyPressed(ebiten.KeyS) || ebiten.IsKeyPressed(ebiten.KeyArrowDown)) && p.Y < p.RH-p.Height {
+	} else if (ebiten.IsKeyPressed(ebiten.KeyS) || ebiten.IsKeyPressed(ebiten.KeyArrowDown)) && p.Y < p.RH-p.Height {
 		p.Y += velocity
 		p.Angle = 180
 	}
@@ -43,6 +42,17 @@ func (p *Player) Update() {
 			}
 		}
 	}
+	posx, posy := ebiten.CursorPosition()
+	fmt.Println(ebiten.CursorPosition())
+	fmt.Println(float64(posx-p.X), " ", float64(p.Y-posy))
+	vx, vy := float64(p.X-posx), float64(p.Y-posy)
+
+	sca := -8*vx + -8*vy
+	ra := math.Sqrt(math.Pow(vx, 2) + math.Pow(vy, 2))
+	ra1 := math.Sqrt(math.Pow(-8, 2) + math.Pow(-8, 2))
+	fmt.Println(int(math.Acos(sca / (ra * ra1))))
+	p.Angle2 = int(math.Acos(sca / (ra * ra1)))
+
 }
 
 func (p *Player) Draw(screen *ebiten.Image) {
@@ -55,7 +65,8 @@ func (p *Player) Draw(screen *ebiten.Image) {
 	//canon
 	op.GeoM.Reset()
 	op.GeoM.Translate(-float64(16)/2, -float64(16)/2)
-	op.GeoM.Rotate(float64(p.Angle%360) * 2 * math.Pi / 360)
+	//fmt.Println(p.Angle2)
+	op.GeoM.Rotate(float64(p.Angle2%360) * 2 * math.Pi / 360)
 	op.GeoM.Translate(float64(p.X), float64(p.Y))
 	screen.DrawImage(p.ImgData1, op)
 }
--- main.go
+++ main.go
@@ -59,7 +59,7 @@ func init() {
 	m = dep.Map{ebiten.NewImage(resolWidth, resolHeight), []*dep.Point{}, nil}
 	img := dep.LoadImg("data/img/tank.png")
 	game = &Game{
-		&dep.Player{resolWidth/2 - 8, resolHeight/2 - 8, 16, 16, resolWidth, resolHeight, &m, img.SubImage(image.Rect(0, 0, 16, 16)).(*ebiten.Image), img.SubImage(image.Rect(16, 0, 32, 16)).(*ebiten.Image), 0},
+		&dep.Player{0, 0, 16, 16, resolWidth, resolHeight, &m, img.SubImage(image.Rect(0, 0, 16, 16)).(*ebiten.Image), img.SubImage(image.Rect(16, 0, 32, 16)).(*ebiten.Image), 0, 0},
 		0,
 	}
 }
